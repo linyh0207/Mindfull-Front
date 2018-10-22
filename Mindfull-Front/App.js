fb95e81b02ad6c0093f727f7ccdaa4e8b8295588
Ardelia Tay <ardeliatay@Ardelias-MacBook-Pro.local>
2018-10-22 16:48:16 -0700

    change route directory to search page

--- Mindfull-Front/App.js
+++ Mindfull-Front/App.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
-import Home from './src/Home.js'
 import Search from './src/Search.js'
 import Reminder from './src/Reminder.js'
 import ObjectRecognition from './src/ObjectRecognition.js'  
 
 const RootStack = createStackNavigator(
   {
-    Home: {
-      screen: Home,
-    },
     Search: {
       screen: Search,
     },
@@ -23,7 +19,7 @@ const RootStack = createStackNavigator(
     },
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Search',
     },
 );
 
