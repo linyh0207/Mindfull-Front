03f609e85c238fb46784ad71a8a3914f094c61c1
Ardelia Tay <ardeliatay@Ardelias-MacBook-Pro.local>
2018-10-27 16:12:54 -0700

    modal for login form and navigation to search page

--- Mindfull-Front/src/Index.js
+++ Mindfull-Front/src/Index.js
@@ -4,8 +4,11 @@ import {
   View,
   Button,
   TextInput,
-  StyleSheet
+  StyleSheet,
+  Modal,
+  TouchableHighlight
 } from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown';
 
 
 class Index extends React.Component {
@@ -19,45 +22,134 @@ class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: ''
+      username: '',
+      modalVisible: false,
+      loginModalVisible: false,
      
     }
 
   };
+
+  signupModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  setLoginModalVisible(visible) {
+    this.setState({loginModalVisible: visible});
+  }
+
+
   
   render() {
     const { navigate } = this.props.navigation;
     console.log(this.state.username)
     return (
       <View style={styles.container}>
-      <Text style={{fontSize: 27}}>Sign Up</Text>
-      <TextInput placeholder='Username' />
-      <TextInput placeholder='Password' />
-      <Button title="Sign Up"
-      />
-      <Text style={{fontSize: 27}}>Login</Text>
+
+          <View>
+            <Modal
+              animationType="slide"
+              transparent={false}
+              visible={this.state.modalVisible}
+            >
+              <View style={{
+              flex: 1,
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              }}>
+
+              <View style={styles.modal}>
+                <TextInput placeholder='Username' />
+                <TextInput placeholder='Password' />
+                <TextInput placeholder='Password Confirmation' />
+                <TouchableHighlight
+                  onPress={() => {
+                    this.signupModalVisible(!this.state.modalVisible);
+                  }}>
+                  <Text style={{fontSize: 27}}>Sign Up</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+            </Modal>
+
+            <TouchableHighlight
+              onPress={() => {
+              this.signupModalVisible(true);
+              }}>
+              <Text>Sign Up</Text>
+            </TouchableHighlight>
+        </View>
+
+
+<TouchableHighlight
+            onPress={() => {
+              this.setLoginModalVisible(true);
+            }}>
+            <Text>Login</Text>
+          </TouchableHighlight>
+      <View>
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={this.state.loginModalVisible}
+          >
+
+            <View style={{
+              flex: 1,
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+
+              <View style={styles.modal}>
                 <TextInput 
-                placeholder='Username' 
-                onChangeText={(username) => this.setState({username})}
-                value={this.state.username}
+                  placeholder='Username' 
+                  onChangeText={(username) => this.setState({username})}
+                  value={this.state.username}
                 />
                 <TextInput placeholder='Password' />
-                <View style={{margin:7}} />
-                <Button 
-                 raised
-                 color='black'
-                 buttonStyle={{backgroundColor: 'rgb(250,188,87)', borderRadius: 10, padding: 10, marginBottom: 20, width: 300}}
-                 textStyle={{textAlign: 'center'}}
-                          onPress={() => this.props.navigation.navigate('Search', {username: this.state.username})}
-                          title="Submit"
-              
-                      />
+                <TouchableHighlight>
+                  <Button 
+                  raised
+                  color='black'
+                  buttonStyle={{backgroundColor: 'rgb(250,188,87)', borderRadius: 10, padding: 10, marginBottom: 20, width: 300}}
+                  textStyle={{textAlign: 'center'}}
+
+                    
+                    onPress={() => 
+                      
+                      navigate('Search', {username: this.state.username})
+                      }
+                    title="Submit"
+                  />
+                  </TouchableHighlight>
+
+                  <TouchableHighlight
+                onPress={() => {
+                  this.setLoginModalVisible(!this.state.loginModalVisible);
+                }}>
+                <Text>X</Text>
+              </TouchableHighlight>
                  
+                </View>
+              </View>
+            </Modal>
+
+        
+        </View>
+
+
+
+
+
 
       </View>
     );
   };
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -65,6 +157,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modal: {
+    marginTop: 22, 
+    padding: 20, 
+    width: 300, 
+    backgroundColor: 'white', 
+    justifyContent: 'center'
   },
 }); 
 
